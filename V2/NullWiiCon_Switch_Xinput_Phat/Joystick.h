0d451d18bea991a202aa2b115c987873a98783ba
nullstalgia <nullstalgia@gmail.com>
2021-07-30 14:59:47 -0700

    Version 2.2 PCB, updated code, moved older stuff

---
+++ V2/NullWiiCon_Switch_Xinput_Phat/Joystick.h
@@ -0,0 +1,181 @@
+/*
+             LUFA Library
+     Copyright (C) Dean Camera, 2014.
+
+  dean [at] fourwalledcubicle [dot] com
+           www.lufa-lib.org
+*/
+
+/*
+  Copyright 2014  Dean Camera (dean [at] fourwalledcubicle [dot] com)
+
+  Permission to use, copy, modify, distribute, and sell this
+  software and its documentation for any purpose is hereby granted
+  without fee, provided that the above copyright notice appear in
+  all copies and that both that the copyright notice and this
+  permission notice and warranty disclaimer appear in supporting
+  documentation, and that the name of the author not be used in
+  advertising or publicity pertaining to distribution of the
+  software without specific, written prior permission.
+
+  The author disclaims all warranties with regard to this
+  software, including all implied warranties of merchantability
+  and fitness.  In no event shall the author be liable for any
+  special, indirect or consequential damages or any damages
+  whatsoever resulting from loss of use, data or profits, whether
+  in an action of contract, negligence or other tortious action,
+  arising out of or in connection with the use or performance of
+  this software.
+*/
+
+/** \file
+ *
+ *  Header file for Joystick.c.
+ */
+
+#ifndef _JOYSTICK_H_
+#define _JOYSTICK_H_
+
+#define bit_set(p,m) ((p) |= (m))
+#define bit_clear(p,m) ((p) &= ~(m))
+#define bit_write(c,p,m) (c ? bit_set(p,m) : bit_clear(p,m))
+#define bit_check(value, bit) (((value) >> (bit)) & 0x01)
+#define bit_toggle(value, bit) ((value) ^= (1 << bit))
+
+#define DPAD_UP_MASK_ON 0x00
+#define DPAD_UPRIGHT_MASK_ON 0x01
+#define DPAD_RIGHT_MASK_ON 0x02
+#define DPAD_DOWNRIGHT_MASK_ON 0x03
+#define DPAD_DOWN_MASK_ON 0x04
+#define DPAD_DOWNLEFT_MASK_ON 0x05
+#define DPAD_LEFT_MASK_ON 0x06
+#define DPAD_UPLEFT_MASK_ON 0x07
+#define DPAD_NOTHING_MASK_ON 0x08
+#define A_MASK_ON 0x04
+#define B_MASK_ON 0x02
+#define X_MASK_ON 0x08
+#define Y_MASK_ON 0x01
+#define LB_MASK_ON 0x10
+#define RB_MASK_ON 0x20
+#define ZL_MASK_ON 0x40
+#define ZR_MASK_ON 0x80
+#define START_MASK_ON 0x200
+#define SELECT_MASK_ON 0x100
+#define SWITCH_LCLICK 0x400
+#define SWITCH_RCLICK 0x800
+#define HOME_MASK_ON 0x1000
+#define CAPTURE_MASK_ON 0x2000
+
+#define BUTTONUP 0
+#define BUTTONDOWN 1
+#define BUTTONLEFT 2
+#define BUTTONRIGHT 3
+#define BUTTONA 4
+#define BUTTONB 5
+#define BUTTONX 6
+#define BUTTONY 7
+#define BUTTONLB 8
+#define BUTTONRB 9
+#define BUTTONZL 10
+#define BUTTONZR 11
+#define BUTTONSTART 12
+#define BUTTONSELECT 13
+#define BUTTONHOME 14
+#define BUTTONCAPTURE 15
+#define LEFT_CLICK 16
+#define RIGHT_CLICK 17
+
+// digital_buttons_1
+#define XBOX_DPAD_UP		0x01
+#define XBOX_DPAD_DOWN		0x02
+#define XBOX_DPAD_LEFT		0x04
+#define XBOX_DPAD_RIGHT		0x08
+#define XBOX_START			0x10
+#define XBOX_BACK			0x20
+#define XBOX_LEFT_STICK		0x40
+#define XBOX_RIGHT_STICK	0x80
+
+// digital_buttons_2
+#define XBOX_LB		0x01
+#define XBOX_RB		0x02
+#define XBOX_HOME	0x04
+#define XBOX_A		0x10
+#define XBOX_B		0x20
+#define XBOX_X		0x40
+#define XBOX_Y		0x80
+
+/* Includes: */
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include <avr/power.h>
+#include <avr/interrupt.h>
+#include <string.h>
+
+#include "Descriptors.h"
+
+#include <LUFA/LUFA/Drivers/USB/USB.h>
+#include <LUFA/LUFA/Drivers/Board/Joystick.h>
+#include <LUFA/LUFA/Drivers/Board/LEDs.h>
+#include <LUFA/LUFA/Drivers/Board/Buttons.h>
+#include <LUFA/LUFA/Platform/Platform.h>
+
+
+// Joystick HID report structure. We have an input and an output.
+typedef struct {
+	uint16_t Button; // 16 buttons; see JoystickButtons_t for bit mapping
+	uint8_t  HAT;    // HAT switch; one nibble w/ unused nibble
+	uint8_t  LX;     // Left  Stick X
+	uint8_t  LY;     // Left  Stick Y
+	uint8_t  RX;     // Right Stick X
+	uint8_t  RY;     // Right Stick Y
+	uint8_t  VendorSpec;
+} USB_SwitchJoystickReport_Input_t;
+extern USB_SwitchJoystickReport_Input_t SwitchReportData;
+
+// The output is structured as a mirror of the input.
+// This is based on initial observations of the Pokken Controller.
+typedef struct {
+	uint16_t Button; // 16 buttons; see JoystickButtons_t for bit mapping
+	uint8_t  HAT;    // HAT switch; one nibble w/ unused nibble
+	uint8_t  LX;     // Left  Stick X
+	uint8_t  LY;     // Left  Stick Y
+	uint8_t  RX;     // Right Stick X
+	uint8_t  RY;     // Right Stick Y
+} USB_SwitchJoystickReport_Output_t;
+
+typedef struct {
+	uint8_t rid;
+	uint8_t rsize;
+	uint8_t digital_buttons_1;
+	uint8_t digital_buttons_2;
+	uint8_t lt;
+	uint8_t rt;
+	int l_x;
+	int l_y;
+	int r_x;
+	int r_y;
+	uint8_t reserved_1[6];
+} USB_XInputJoystickReport_Data_t;
+
+extern USB_XInputJoystickReport_Data_t XInputReportData;
+
+
+/* Function Prototypes: */
+#ifdef __cplusplus
+extern "C" {
+#endif
+void xbox_reset_pad_status(void);
+// Setup all necessary hardware, including USB initialization.
+void SetupHardware(void);
+// Process and deliver data from IN and OUT endpoints.
+void HID_Task(void);
+// USB device event handlers.
+void EVENT_USB_Device_Connect(void);
+void EVENT_USB_Device_Disconnect(void);
+void EVENT_USB_Device_ConfigurationChanged(void);
+void EVENT_USB_Device_ControlRequest(void);
+// Prepare the next report for the host.
+#ifdef __cplusplus
+}
+#endif
+#endif
